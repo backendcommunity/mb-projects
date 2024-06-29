fbaf5453ba5bd6509528d59dbf4950da64d51320
Solomon Eseme <kaperskyguru@gmail.com>
2024-06-29 19:28:12 +0100

    fix: update design

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -1,24 +1,74 @@
 <template>
   <div>
-    <div class="px-10 pt-20 md:pt-40"></div>
+    <div class="px-10 pt-20 md:pt-40">
+      <nuxt-link
+        to="#"
+        class="flex items-center gap-2 text-secondary"
+        @click="back"
+        ><svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M24 13.3a.2.2 0 01-.2.2H5.74l8.239 8.239a.2.2 0 010 .282L12.14 23.86a.2.2 0 01-.282 0L.14 12.14a.2.2 0 010-.282L11.86.14a.2.2 0 01.282 0L13.98 1.98a.2.2 0 010 .282L5.74 10.5H23.8c.11 0 .2.09.2.2v2.6z"
+            fill="currentColor"
+          ></path>
+        </svg>
+        <span> Back</span></nuxt-link
+      >
+    </div>
 
     <section class="bg-white mb-20 max-w-7xl mx-auto px-5">
       <h1 class="text-4xl text-primary text-center font-bold">
         {{ project?.title }}
       </h1>
       <div class="lg:grid grid-cols-7 my-10 flex flex-col gap-10">
-        <div class="col-span-5 p-">
-          <article
-            data-v-3d56a840=""
-            data-clarity-region="article"
-            class="w-full h-full"
-            v-html="project?.description"
-          ></article>
+        <div class="col-span-5 flex flex-col gap-10">
+          <div class="p-5 shadow w-full h-full">
+            <article
+              id="article"
+              data-v-3d56a840=""
+              data-clarity-region="article"
+              class="w-full h-full"
+              v-html="project?.description"
+            ></article>
+          </div>
+          <div class="shadow p-5">
+            <article id="article">
+              <h2>Join our community</h2>
+              <p>
+                Need to show-off or ask doubts?
+                <a href="https://masteringbackend.com/community"
+                  >Join our Slack Community</a
+                >. Ask questions, help others and learn in public to make the
+                best use of MBProject.
+              </p>
+            </article>
+          </div>
         </div>
 
         <div class="self-start sticky top-40 col-span-2 md:w-full">
-          <div class="h-full w-full shadow p-5">
-            <div class="w-full bg-white mb-5">
+          <div class="h-full w-full shadow rounded bg-primary p-5">
+            <div class="text-white mb-5 border-b pb-6">
+              <h2 class="text-2xl py-3">Ready? Start Building</h2>
+              <p class="py-5">
+                Includes the necessary assets, design files, style guide and a
+                README file to help you with each step of the project.
+              </p>
+
+              <a
+                :href="project?.prd ?? '#'"
+                target="_blank"
+                class="px-6 py-3 w-full bg-secondary text-white text-center font-bold text-lg rounded block"
+              >
+                Start Building
+              </a>
+            </div>
+
+            <div class="w-full mb-5">
               <div class="flex">
                 <div
                   class="text-center w-full justify-center flex flex-col gap-3 p-2"
@@ -26,7 +76,7 @@
                   <a
                     :href="project?.prd ?? '#'"
                     target="_blank"
-                    class="px-6 py-3 border border-solid w-full bg-secondary text-white font-bold text-lg rounded block"
+                    class="px-2 py-1 w-full border border-secondary border-solid text-secondary font-bold text-lg rounded block"
                   >
                     Download PRD
                   </a>
@@ -34,67 +84,10 @@
                   <a
                     :href="project?.frontend ?? '#'"
                     target="_blank"
-                    class="px-6 py-3 border border-secondary border-solid w-ful text-secondary font-bold text-lg rounded block"
+                    class="px-2 py-1 border border-secondary border-solid w-ful text-secondary font-bold text-lg rounded block"
                   >
                     Preview Frontend
                   </a>
-                </div>
-              </div>
-            </div>
-
-            <div class="w-full bg-white">
-              <div class="grid grid-cols-2 w-full gap-5">
-                <div class="flex items-center gap-2">
-                  <span class="w-6">
-                    <img
-                      class="star-rating"
-                      src="~/assets/img/blue-check.png"
-                      alt=""
-                    />
-                  </span>
-                  <div class="flex flex-1 flex-col">
-                    <h3 class="text-lg leading-6">{{ project.level }}</h3>
-                  </div>
-                </div>
-                <div class="flex items-center gap-2">
-                  <span class="w-6">
-                    <img
-                      class="star-rating"
-                      src="~/assets/img/blue-check.png"
-                      alt=""
-                    />
-                  </span>
-                  <div class="flex flex-1 flex-col">
-                    <h3 class="text-lg leading-6">2 Tasks</h3>
-                  </div>
-                </div>
-
-                <div class="flex items-center gap-2">
-                  <span class="w-6">
-                    <img
-                      class="star-rating"
-                      src="~/assets/img/blue-check.png"
-                      alt=""
-                    />
-                  </span>
-                  <div class="flex flex-1 flex-col">
-                    <h3 class="text-lg leading-6">{{ project.duration }}</h3>
-                  </div>
-                </div>
-
-                <div class="flex items-center gap-2">
-                  <span class="w-6">
-                    <img
-                      class="star-rating"
-                      src="~/assets/img/blue-check.png"
-                      alt=""
-                    />
-                  </span>
-                  <div class="flex flex-1 flex-col">
-                    <h3 class="text-lg leading-6 w-full">
-                      {{ project.category }}
-                    </h3>
-                  </div>
                 </div>
               </div>
             </div>
@@ -102,7 +95,48 @@
 
           <div class="w-full my-5 bg-white shadow p-5">
             <h4 class="text-2xl text-bold mb-5">Tags</h4>
+
             <div class="grid grid-cols-2 w-full gap-5">
+              <div class="flex items-center gap-2">
+                <span class="w-6">
+                  <img
+                    class="star-rating"
+                    src="~/assets/img/blue-check.png"
+                    alt=""
+                  />
+                </span>
+                <div class="flex flex-1 flex-col">
+                  <h3 class="text-lg leading-6">{{ project.level }}</h3>
+                </div>
+              </div>
+              <div class="flex items-center gap-2">
+                <span class="w-6">
+                  <img
+                    class="star-rating"
+                    src="~/assets/img/blue-check.png"
+                    alt=""
+                  />
+                </span>
+                <div class="flex flex-1 flex-col">
+                  <h3 class="text-lg leading-6">2 Tasks</h3>
+                </div>
+              </div>
+
+              <div class="flex items-center gap-2">
+                <span class="w-6">
+                  <img
+                    class="star-rating"
+                    src="~/assets/img/blue-check.png"
+                    alt=""
+                  />
+                </span>
+                <div class="flex flex-1 flex-col">
+                  <h3 class="text-lg leading-6 w-full">
+                    {{ project.category }}
+                  </h3>
+                </div>
+              </div>
+
               <div
                 class="flex items-center gap-2"
                 v-for="(item, i) in project?.tags"
@@ -147,91 +181,93 @@
 const { getProject } = await useProjects("/projects");
 const project = await getProject(useRoute().params.slug);
 
-console.log(project);
+function back() {
+  useRouter().back();
+}
 </script>
   
-  <style scoped>
-article img {
+  <style >
+#article img {
   height: auto;
   width: 100%;
   background-size: cover;
 }
 
-article table {
+#article table {
   border: 1px solid #eee;
 }
 
-article > * {
+#article > * {
   font-family: "Georgia", serif !important;
 }
 
-article p,
-article {
+#article p,
+#article {
   margin-bottom: 1.3rem;
   font-weight: 400;
   line-height: 2;
 }
 
-article h1,
-article h2,
-article h3,
-article h4,
-article h5,
-article h6 {
+#article h1,
+#article h2,
+#article h3,
+#article h4,
+#article h5,
+#article h6 {
   padding-bottom: 0.8rem;
   padding-top: 0.8rem;
   font-weight: bold;
 }
 
-article h2 {
+#article h2 {
+  font-size: 1.8rem;
+}
+
+#article h3 {
   font-size: 1.6rem;
 }
 
-article h3 {
+#article h4 {
   font-size: 1.4rem;
 }
 
-article h4 {
+#article h5 {
   font-size: 1.2rem;
 }
 
-article h5 {
+#article h6 {
   font-size: 1rem;
 }
 
-article h6 {
-  font-size: 0.8rem;
-}
-
-article > ul {
+#article > ul {
   list-style: disc !important;
 }
 
-article > ol {
+#article > ol {
   list-style: decimal !important;
 }
-article ul,
-article ol {
+#article ul,
+#article ol {
   margin-bottom: 1.3rem;
   margin-bottom: 0.5rem;
   padding-left: 1.5rem;
 }
 
-article ul li,
-article ol li {
+#article ul li,
+#article ol li {
   line-height: 32px;
   font-size: 1.125rem;
   font-weight: 400;
 }
 
-article figure figcaption {
+#article figure figcaption {
   line-height: 32px;
   text-align: center;
   font-style: italic;
   font-weight: 400;
 }
 
-article figure img {
+#article figure img {
   outline: none !important;
   margin: 0 auto !important;
   border: 5px solid #cccccc;
@@ -241,8 +277,8 @@ article figure img {
   border-radius: 0 !important;
 }
 
-article a,
-article a:hover {
+#article a,
+#article a:hover {
   color: #191488;
   transition: all 0.2s ease-in-out 0s;
   background-color: transparent;
